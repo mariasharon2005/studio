f97ef534089d88739132b6231981fcbd74900bab
MARIA SHARON <sharonmariachs@gmail.com>
2026-03-27 10:11:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1967716379-c89e8",
+  "appId": "1:219490130838:web:26510dba0bd2a7e4959bb7",
+  "apiKey": "AIzaSyCgXSdpQU94DrEgBNpCXQ8EQX4-aFVGOHA",
+  "authDomain": "studio-1967716379-c89e8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "219490130838"
+};
